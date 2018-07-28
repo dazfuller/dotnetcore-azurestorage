b6c14f9ba31f57061f8f2d61875b9e87a802a462
Darren Fuller <daz.fuller@gmail.com>
2018-07-28 22:08:03 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace dotnetblob
+{
+    public static class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+            
+            var demo = new StorageDemo(config);
+            await demo.Run();
+        }
+    }
+
+    public class StorageDemo
+    {
+        private IConfigurationRoot _config;
+
+        public StorageDemo(IConfigurationRoot config)
+        {
+            _config = config;
+        }
+
+        public async Task Run()
+        {
+            var credentials = new StorageCredentials(_config["Storage:Name"], _config["Storage:Key"]);
+            var storageAccount = new CloudStorageAccount(credentials, useHttps: true);
+            var blobClient = storageAccount.CreateCloudBlobClient();
+
+            var container = blobClient.GetContainerReference(_config["Storage:Container"]);
+            if (!await container.ExistsAsync())
+            {
+                await container.CreateAsync();
+            }
+
+            var blob = container.GetBlockBlobReference("demo_file.txt");
+            await blob.DeleteIfExistsAsync();
+
+            var demoText = "This is some sample text to go into our new blob";
+            using (var inputStream = new MemoryStream(Encoding.UTF8.GetBytes(demoText)))
+            {
+                using (var blobOutputStream = await blob.OpenWriteAsync())
+                {
+                    inputStream.CopyTo(blobOutputStream);
+                }
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,29 @@
+# DotNetCore - Azure Storage Demo
+
+This is a sample application showing how you can use [dotnet core](https://dotnet.github.io/) to write data to [Azure Storage](https://docs.microsoft.com/azure/storage/) using blobs.
+
+To use the application you will need to create an `appsettings.json` file in the root with the following structure.
+
+```json
+{
+    "Storage": {
+        "Name": "<Name of the Storage Account>",
+        "Key": "<Storage key>",
+        "Container": "<container name>"
+    }
+}
+```
+
+Such as:
+
+```json
+{
+    "Storage": {
+        "Name": "dotnetcoredemo",
+        "Key": "54cO0hPm5U6o2RA2exxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx==",
+        "Container": "demo"
+    }
+}
+```
+
+You can get these values using Powershell, Azure CLI or the Azure Portal.
